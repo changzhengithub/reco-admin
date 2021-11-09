ad5c82c5d0f3c57e42d654d586b7f52eff47c7d4
changzhengithub <1979250385@qq.com>
2021-11-09 10:37:51 +0800

    优化框架

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import '@/assets/css/reset.css'
 
 import '@/permission' // 权限设置
-import directive from '@/utils/directive' // 自定义指令
+import directives from '@/utils/directives' // 自定义指令
 
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'; // 引入图标库
@@ -18,7 +18,7 @@ app.use(store);
 app.use(router);
 app.use(ArcoVue);
 app.use(ArcoVueIcon);
-app.use(directive);
+app.use(directives);
 app.mount('#app');
 
 // createApp(App).use(store).use(router).mount('#app')
